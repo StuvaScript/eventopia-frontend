1fd5ac09e19bc56b0543aa4f42bf1e0ae7bc5f87
Stu <stumac1000@yahoo.com>
2024-12-07 16:02:28 -0800

    Update Axios request request file, Stu_12.6.24
    Add a POST and DELETE method for requesting data.
    Include fromatting examples params, requestBody,
    and headers.

--- src/util/index.js
+++ src/util/index.js
@@ -1,25 +1,75 @@
 import axios from "axios";
- 
+
+// ``** GET **``
+
+// GET params example
+const params = {
+  params: {
+    ID: 12345,
+  },
+};
+
 // note: not used, but could be used with GET with params
- const getData = async (url, params) => {
-    try {
-      let res = await axios.get(url, params);
-      let data = await res.data;
-      return data;
-    } catch (error) {
-      console.log(error, `error - getData in ${url} route`);
-    }
-  };
+const getData = async (url, params) => {
+  try {
+    let res = await axios.get(url, params);
+    let data = await res.data;
+    return data;
+  } catch (error) {
+    console.log(error, `error - getData in ${url} route`);
+  }
+};
 
-  const getAllData = async (url) => {
-    try {
-      let res = await axios.get(url);
-      let data = await res.data;
-      return data;
-    } catch (error) {
-      console.log(error, `error - getAllData in ${url} route`);
-    }
-  };
+const getAllData = async (url) => {
+  try {
+    let res = await axios.get(url);
+    let data = await res.data;
+    return data;
+  } catch (error) {
+    console.log(error, `error - getAllData in ${url} route`);
+  }
+};
 
+// ``** POST **``
 
-  export {getData, getAllData};
+// requestBody example
+const requestBody = {
+  data: {
+    firstName: "Finn",
+    lastName: "Williams",
+  },
+};
+
+// headers example
+const headers = {
+  headers: {
+    myCustomHeader1: "value1",
+    myCustomHeader2: "value2",
+    "content-type": "application/json",
+    authorization: "Bearer your_token",
+  },
+};
+
+const postData = async (url, requestBody, headers) => {
+  try {
+    let res = await axios.post(url, requestBody, headers);
+    let data = await res.data;
+    return data;
+  } catch (error) {
+    console.log(error, `error - getData in ${url} route`);
+  }
+};
+
+// ``** DELETE **``
+
+const deleteData = async (url, requestBody, headers) => {
+  try {
+    let res = await axios.delete(url, requestBody, headers);
+    let data = await res.data;
+    return data;
+  } catch (error) {
+    console.log(error, `error - getData in ${url} route`);
+  }
+};
+
+export { getData, getAllData, postData, deleteData };
