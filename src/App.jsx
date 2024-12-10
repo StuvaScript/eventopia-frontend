e48963cd347e7122ec672193493c1cf7b8b80189
Stu <stumac1000@yahoo.com>
2024-12-09 18:03:44 -0800

    Update url and params

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAllData } from "./util/index";
+import { getAllData, getData } from "./util/index";
 import axios from "axios";
 
 const city = "Seattle";
@@ -8,14 +8,31 @@ const startDate = "";
 const endDate = "2025-02-22";
 
 // const URL = "/api/v1/";
-const URL = `/api/events/${city}/${stateCode}/${startDate}/${endDate}`;
+// const URL = `/api/events/${city}/${stateCode}/${startDate}/${endDate}`;
+// const URL = `/api/events/`;
+const URL = `/api/events/${city}/${stateCode}`;
+
+const config = {
+  params: {
+    // city: "Seattle",
+    // stateCode: "WA",
+    startDateTime: "",
+    endDateTime: "2025-02-22",
+  },
+};
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
+    // (async () => {
+    //   const myData = await getAllData(URL);
+    //   // setMessage(myData.data);
+    //   console.log(myData);
+    // })();
+
     (async () => {
-      const myData = await getAllData(URL);
+      const myData = await getData(URL, config);
       // setMessage(myData.data);
       console.log(myData);
     })();
