55423948e0bc319dad24f3795e686bc631aa0a46
nihalerdal <nihalerdall@gmail.com>
2024-12-12 18:45:05 -0800

    Resolve merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import CustomDatePicker from './DatePicker'; 
-import { getAllData } from './util/index';
+import React, { useState, useEffect } from "react";
+import { getAllData } from "./util/index";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./Theme";
+import HomePage from "./pages/HomePage";
 
-const URL = 'http://localhost:8000/api/v1/';
+// const URL = "http://localhost:8000/api/v1/";
+const URL = "/api/v1/";
 
 function App() {
+ const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const myData = await getAllData(URL);
+      setMessage(myData.data);
+    })();
+
+    return () => {
+      console.log("unmounting");
+    };
+  }, []);
 
   return (
-      <CustomDatePicker
-      />
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HomePage />
+      </ThemeProvider>
+    </>
   );
-
 }
 
-export default App
+export default App;
