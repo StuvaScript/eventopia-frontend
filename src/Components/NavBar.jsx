e595ff2367241be71193867e064df658b01ede31
Stu <stumac1000@yahoo.com>
2025-01-05 18:57:53 -0800

    Adjust Ticketmaster URL to reflect backend changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -34,9 +34,9 @@ const NavBar = ({ title }) => {
   };
 
   const handleSearch = async () => {
-    const URL = `${
-      import.meta.env.VITE_API_BASE_URL
-    }/api/v1/ticketmaster/events/${location.city}/${location.state}`;
+    const URL = `${import.meta.env.VITE_API_BASE_URL}/api/ticketmaster/events/${
+      location.city
+    }/${location.state}`;
 
     // Optional config
     const config = {
