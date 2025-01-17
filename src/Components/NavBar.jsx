83b75d86da4e2d5c4098d04597082a5cee36c42f
Uma Venki <iamumamsekar@gmail.com>
2025-01-16 17:08:43 -0800

    Updated code after datepicker changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -42,26 +42,12 @@ const NavBar = ({ title }) => {
       location.city
     }/${location.state}`;
 
-    // Populate date range
-    const localStartDate = new Date(`${dateRange[0]}T00:00:00`);
-    const localEndDate = new Date(`${dateRange[1]}T23:59:59`);
-    // Convert local datetime to UTC using toISOString()
-    const dateRangeStart = localStartDate.toISOString().slice(0, 19) + "Z";
-    const dateRangeEnd = localEndDate.toISOString().slice(0, 19) + "Z";
-
-    // Optional config
     const config = {
       params: {
-        dateRangeStart: dateRangeStart,
-        dateRangeEnd: dateRangeEnd,
+        dateRangeStart: dateRange[0],
+        dateRangeEnd: dateRange[1],
       },
     };
-    // const config = {
-    //   params: {
-    //     dateRangeStart: dateRange[0],
-    //     dateRangeEnd: dateRange[1],
-    //   },
-    // };
 
     console.log("Request URL:", URL);
     console.log("Request Config:", config);
